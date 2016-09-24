277b75a875b8ba8d0d29135ee24595ef44f0efb4
Cole Vahey <rcvahey01@gmail.com>
2016-09-23 21:04:47 -0400

    save

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+console.log("I AM WORKING")
+
+var enterspoter = document.getElementsByClassName("enterspot").innerHTML
+console.log(enterspoter)
+console.log(document.getElementsByClassName("enterspoter").innerText)
