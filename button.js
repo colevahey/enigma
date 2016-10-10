c66cbe3445dfed1da6c1e6c869b5697fb61d906c
Cole Vahey <rcvahey01@gmail.com>
2016-10-10 18:35:15 -0400

    save

--- button.js
+++ button.js
@@ -1,10 +1,12 @@
 "use strict"
 
-var buttonhit = function(json){
+var buttonhit = function(json,Def0,Def1,Def2,Def3,Def4,Def5,Def6,Def7,Def8,Def9){
   var message = document.getElementById("enterbar").value
   if (message !== ""){
-    var cool = Math.floor(Math.random()*10)
-    console.log(cool)
+    var rand = Math.floor(Math.random()*10)
+    if (rand == 0){
+      console.log("IT WAS DEF0")
+    }
     run(message, json)
   } else {
     console.log("There is no message");
