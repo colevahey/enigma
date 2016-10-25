66ab0cc35995d1a90481fb08dd0355c32d7526ad
Cole Vahey <rcvahey01@gmail.com>
2016-10-25 09:30:31 -0400

    save

--- button.js
+++ button.js
@@ -43,3 +43,9 @@ var copytoclip = function (){
   var cool = document.getElementById("encodedmessage").innerHTML
   copy(cool)
 }
+
+function myFunction() {
+  var encodedmessage = document.getElementById("encodedmessage").innerHTML;
+  var cln = encodedmessage.cloneNode(true);
+  console.log(cln)
+}
