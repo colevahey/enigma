17ac9afdf9985e95ea580f2826a3ffa17c3c0cc2
Cole Vahey <rcvahey01@gmail.com>
2016-10-27 09:13:22 -0400

    save

--- buttons.js
+++ buttons.js
@@ -3,40 +3,35 @@
 var buttonhit = function(json){
   var message = document.getElementById("enterbar").value
   if (message !== ""){
-    run(message, json)
+    var finalmessage = ""
+    if (message.startsWith("#")){
+      var ret = {};
+      for(var key in json){
+        ret[json[key]] = key
+      }
+      for (var n = 1; n < message.length; n++){
+        var jsonindex = message[n]
+        if (jsonindex in json) {
+          finalmessage += ret[jsonindex]
+        } else {
+          finalmessage += jsonindex
+        }
+      }
+    } else {
+      finalmessage += "#"
+      for (var n = 0; n < message.length; n++){
+        var jsonindex = message[n]
+        if (jsonindex in json) {
+          finalmessage += json[jsonindex]
+        } else {
+          finalmessage += jsonindex
+        }
+      }
+    }
+    document.getElementById("encodedmessage").innerHTML = finalmessage
   } else {
-    console.log("There is no message");
     document.getElementById("encodedmessage").innerHTML = 'Message'
   }
-}
-
-var run = function(message, json){
-  var finalmessage = ""
-  if (message.startsWith("#")){
-    var ret = {};
-    for(var key in json){
-      ret[json[key]] = key
-    }
-    for (var n = 1; n < message.length; n++){
-      var jsonindex = message[n]
-      if (jsonindex in json) {
-        finalmessage += ret[jsonindex]
-      } else {
-        finalmessage += jsonindex
-      }
-    }
-  } else {
-    finalmessage += "#"
-    for (var n = 0; n < message.length; n++){
-      var jsonindex = message[n]
-      if (jsonindex in json) {
-        finalmessage += json[jsonindex]
-      } else {
-        finalmessage += jsonindex
-      }
-    }
-  }
-  document.getElementById("encodedmessage").innerHTML = finalmessage
 }
 
 var copytoclip = function (){
