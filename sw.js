b6e1331268569e58e277b0018dd2bb17d3edad83
Cole Vahey <rcvahey01@gmail.com>
2017-04-25 08:56:46 -0400

    Working hard on service workers

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-/** An empty service worker! */
+importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open('airhorner').then(function(cache) {
+return cache.addAll([
+'./',
+'./index.html',
+'./currentcode.js',
+'./code',
+'./main.css',
+'./site.js',
+'./manifest.json',
+'./README.md',
+'./assets/enigmalogo.gif',
+'./assets/background.svg',
+'./assets/copytoclip.png',
+'./assets/icon.png'
+]);
+})
+);
+});
