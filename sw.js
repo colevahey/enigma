5b474a5247df2cad431c05d12b06f7e3da977210
Cole Vahey <rcvahey01@gmail.com>
2017-04-27 08:38:52 -0400

    save

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('colevahey.github.io/enigma').then(function(cache) {
+      return cache.addAll([
+        './main.css'
+        // etc
+      ]);
+    })
+  );
+});
